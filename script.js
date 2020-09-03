9696291e43da992fddd969ee21cf869207efd9e0
HusseinAlsakkaf <64165255+HusseinAlsakkaf@users.noreply.github.com>
2020-09-03 07:02:22 +0100

    Dropdownlist not working

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
-//You can edit ALL of the code here
-/* function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-  
-}
-
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-  
-} */
-
-//window.onload = setup; 
-//=======================================================================
-
+//variables declaration
+var allEpisodes = getAllEpisodes();
 var episodeFrames = document.getElementById('episodeFrames');
 var searchBar = document.getElementById('searchBar');
-var allEpisodes = getAllEpisodes();
 var episodeCode = "";
 var searchResults = document.getElementById('searchResults');
+var selectList = document.getElementById("selects");
 
 
 
-searchBar.addEventListener('keyup', function(e){
-  var searchTerm = e.target.value.toLowerCase();
-  
-  var filteredEpisodes = allEpisodes.filter((episode) => {
+// Live Search 
+ searchBar.addEventListener('keyup', function(e){
+  var searchTerm = e.target.value.toLowerCase();  
+   var filteredEpisodes = allEpisodes.filter((episode) => {
   
     return (
       episode.name.toLowerCase().includes(searchTerm) ||
@@ -33,25 +19,42 @@ searchBar.addEventListener('keyup', function(e){
     );
     
   });
-
-  displayCharacters(filteredEpisodes);
+   
+  displayEpisodes(filteredEpisodes);
   
-});
+}); 
 
-/* const loadCharacters = async () => {
-  try {
-    const res = await fetch('https://api.tvmaze.com/shows/82/episodes');
-    allEpisodes = await res.json();
-    displayCharacters(allEpisodes);
-  } catch (err) {
-    console.error(err);
-  }
-}; */
 
-const displayCharacters = (episodes) => {
+// display episods function
+const displayEpisodes = (filteredEpisodes) => {
+
+ /*
+  this code is not working however it is supposed
+ to store -the episode that was clicked from the dropdown list- it is supposed to store it 
+ in the "filteredEpisodes" variable. This way the code will only display the selected episode 
+ from the dropdown list 
+   */
+  selectList.onchange = function () {
+
+     filteredEpisodes = allEpisodes.filter((episode) => {
+
+      this.value.toLowerCase() == episode.name.toLowerCase();
+    });  
+
+
+  }  
  
-  const htmlString = episodes.map((episodes) => {
+  // using map to populate the page with the filtered episodes
+  const htmlString = filteredEpisodes.map((episodes) => {
+// creating a dropdown list
+    var option = document.createElement("option");
+    option.value = episodes.name;
+    option.text = episodes.name;
+    selectList.appendChild(option);
      
+  
+// episodes format code
+
       if (episodes.number <= 9 && episodes.season <= 9) {
         episodeCode = `S0${episodes.season}E0${episodes.number}`;
       } else if (episodes.number <= 9 && episodes.season > 9) {
@@ -61,23 +64,30 @@ const displayCharacters = (episodes) => {
       } else if (episodes.number > 9 && episodes.season > 9) {
         episodeCode = `S${episodes.season}E${episodes.number}`;
       } 
+  
+      
+    
+    // displaying episodes
     
       return `
-            <div class="character">
+            <div>
                 <h2>${episodes.name} ${episodeCode}</h2>
                 <p> ${episodes.summary}</p>
                 <img src="${episodes.image.medium}"></img>
             </div>
         `;
     });
-    //.join('');
-   
-  searchResults.innerHTML = `Displaying ${htmlString.length} of 73`;
+  
+  
+  searchResults.innerHTML = `Displaying ${htmlString.length} of ${allEpisodes.length}`;
   episodeFrames.innerHTML = htmlString;
   
 };
 
-//loadCharacters();
-displayCharacters(allEpisodes);
 
 
+/* calling the display function with all episodes as parameter in order to diplay
+ all the episodes before entering a search term */
+
+displayEpisodes(allEpisodes);
+ 
