35127dc15912074d051abd96e3106d2d7b7da54b
HusseinAlsakkaf <64165255+HusseinAlsakkaf@users.noreply.github.com>
2020-09-09 17:35:14 +0100

    Level 350 completed

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //variables declaration
-var allEpisodes = getAllEpisodes();
 var episodeFrames = document.getElementById('episodeFrames');
 var searchBar = document.getElementById('searchBar');
 var episodeCode = "";
@@ -7,51 +6,53 @@ var searchResults = document.getElementById('searchResults');
 var selectList = document.getElementById("selects");
 var homeBtn = document.getElementById("homeBtn");
 
-// creating a dropdown list
-allEpisodes.map((episodes) => {
-  var option = document.createElement("option");
-  option.value = episodes.name;
-  option.text = episodes.name;
-  selectList.appendChild(option);
-});
+//=============fetch========================
+async function getData(){
+  const response = await fetch("https://api.tvmaze.com/shows/82/episodes");
+  const data = await response.json();
+  const allEpisodes = data;
 
-// Live Search 
- searchBar.addEventListener('keyup', function(el){
-  var searchTerm = el.target.value.toLowerCase();  
-   var filteredEpisodes = allEpisodes.filter((episode) => {
-  
-    return (
-      episode.name.toLowerCase().includes(searchTerm) ||
-      episode.summary.toLowerCase().includes(searchTerm)
-    );
-    
+  //===================
+
+  // creating a dropdown list
+  allEpisodes.map((episodes) => {
+    var option = document.createElement("option");
+    option.value = episodes.name;
+    option.text = episodes.name;
+    selectList.appendChild(option);
   });
-   
-  displayEpisodes(filteredEpisodes);
-  
-}); 
 
+  // Live Search
+  searchBar.addEventListener("keyup", function (el) {
+    var searchTerm = el.target.value.toLowerCase();
+    var filteredEpisodes = allEpisodes.filter((episode) => {
+      return (
+        episode.name.toLowerCase().includes(searchTerm) ||
+        episode.summary.toLowerCase().includes(searchTerm)
+      );
+    });
 
-// display episods function
-const displayEpisodes = (filteredEpisodes) => {
+    displayEpisodes(filteredEpisodes);
+  });
 
- /*
+  // display episods function
+  const displayEpisodes = (filteredEpisodes) => {
+    /*
  This code stores -the episode that was clicked from the dropdown list- it stores it 
  in the "filteredEpisodes" variable. This way the code will only display the selected episode 
  from the dropdown list 
    */
-   selectList.addEventListener("change", function() {
-     filteredEpisodes = allEpisodes.filter(function (e) {
-        return e.name == selectList.value 
-      }); 
+    selectList.addEventListener("change", function () {
+      filteredEpisodes = allEpisodes.filter(function (e) {
+        return e.name == selectList.value;
+      });
 
-     displayEpisodes(filteredEpisodes);
-  }); 
-  
+      displayEpisodes(filteredEpisodes);
+    });
 
-  // using map to populate the page with the filtered episodes
-  const htmlString = filteredEpisodes.map((episodes) => {
-// episodes format code
+    // using map to populate the page with the filtered episodes
+    const htmlString = filteredEpisodes.map((episodes) => {
+      // episodes format code
 
       if (episodes.number <= 9 && episodes.season <= 9) {
         episodeCode = `S0${episodes.season}E0${episodes.number}`;
@@ -61,12 +62,10 @@ const displayEpisodes = (filteredEpisodes) => {
         episodeCode = `S0${episodes.season}E${episodes.number}`;
       } else if (episodes.number > 9 && episodes.season > 9) {
         episodeCode = `S${episodes.season}E${episodes.number}`;
-      } 
-  
-      
-    
-    // displaying episodes
-    
+      }
+
+      // displaying episodes
+
       return `
             <div class = "episodeDiv">
                 <h2 class = "title">${episodes.name} ${episodeCode}</h2>
@@ -76,19 +75,19 @@ const displayEpisodes = (filteredEpisodes) => {
             </div>
         `;
     });
-  
-  
-  searchResults.innerHTML = `test ${htmlString.length} of ${allEpisodes.length}`;
-  episodeFrames.innerHTML = htmlString;
-  
-};
-// Home button calls the display function with "allEpisodes" as a parameter to diplay all episodes again
-homeBtn.addEventListener("click",function(){
-  displayEpisodes(allEpisodes);
-});
 
-/* calling the display function with all episodes as parameter in order to diplay
+    searchResults.innerHTML = `test ${htmlString.length} of ${allEpisodes.length}`;
+    episodeFrames.innerHTML = htmlString;
+  };
+  // Home button calls the display function with "allEpisodes" as a parameter to diplay all episodes again
+  homeBtn.addEventListener("click", function () {
+    displayEpisodes(allEpisodes);
+  });
+
+  /* calling the display function with all episodes as parameter in order to diplay
  all the episodes before entering a search term */
 
-displayEpisodes(allEpisodes);
- 
+  displayEpisodes(allEpisodes);
+}
+
+ getData();
